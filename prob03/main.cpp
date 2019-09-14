6dcfe647059ee8ffaab437cc39d01fa4c7c08ea2
stephanietalley <stephanietalley@cs104u-4h.acad.fullerton.edu>
2019-09-14 14:13:18 -0700

    Created sales prediction calculator

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,21 @@
 // Sales prediction
+
+#include <iostream>
+
+int main()
+{
+  double sales, prediction, increase, new_number;
+
+  increase = 0.18;
+
+  std::cout << "What is last year's total sales? $";
+  std::cin >> sales;
+
+  new_number = sales * increase;
+  prediction = new_number + sales;
+
+  std::cout << "Last year's sales were $" << sales <<std::endl;
+  std::cout << "This year's sales prediction: $" << prediction << std::endl;
+
+  return 0;
+}
